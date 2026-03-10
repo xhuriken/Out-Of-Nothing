9541a68874b1338e684dba561cb799ee13aa7a28
xhuriken <73635763+xhuriken@users.noreply.github.com>
2026-03-10 23:21:45 +0100

    I will starting to make the energy

--- Assets/1_Scripts/3_Interfaces/Energy/IEnergyStorage.cs
+++ Assets/1_Scripts/3_Interfaces/Energy/IEnergyStorage.cs
@@ -13,10 +13,4 @@ public interface IEnergyStorage
     /// </summary>
     /// <returns>The actual amount of energy extracted.</returns>
     float ExtractEnergy(float amount);
-
-    /// <summary>
-    /// Attempts to add energy to the machine.
-    /// </summary>
-    /// <returns>The actual amount of energy added.</returns>
-    float AddEnergy(float amount);
 }
